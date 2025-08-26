8960031cc81bb268f06e6959d1a8914e155fed63
Jose Bennink <jose@mac-4.home>
2025-08-26 10:49:55 +0200

    Update startscherm met achtergrond en knop

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="bg-custom-mobile md:bg-custom-desktop">
-      {/* Je kunt hier nog andere componenten plaatsen als nodig */}
+    <div className="bg-custom w-screen h-screen flex items-center justify-center">
+      <button className="bg-nox-gold text-nox-black px-8 py-4 rounded-full text-xl font-bold shadow-lg hover:scale-105 transition-transform">
+        Start avontuur
+      </button>
     </div>
   );
 }
