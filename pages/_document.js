35fc992dde06a3325a32a08e1b620258f27249e1
Jose Bennink <jose@mac-5.home>
2025-08-27 14:04:11 +0200

    ✨ Add PWA manifest, splashscreens, icons en iOS startup images

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,42 @@
+// /pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="nl">
       <Head>
-        {/* Basisinstellingen voor PWA */}
+        {/* Basis PWA meta */}
         <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
-
-        {/* App-icon voor iOS */}
         <link rel="apple-touch-icon" sizes="180x180" href="/icon-192.png" />
 
-        {/* Splashscreen iPhone 15 */}
+        {/* iOS splashscreens */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/splash/splash-iphoneSE.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/splash/splash-iphone11.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/splash/splash-iphone12.png"
+          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/splash/splash-iphone14promax.png"
+          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/splash/splash-ipad.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+        />
         <link
           rel="apple-touch-startup-image"
           href="/splash/splash-iphone15-optimized.png"
