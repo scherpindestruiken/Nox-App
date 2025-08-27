3721b08b5462496fe5d61c3c55ef33c6f6b32f60
Jose Bennink <jose@mac.home>
2025-08-27 09:13:08 +0200

    Fix splash voor iPhone 15

--- pages/_document.js
+++ pages/_document.js
@@ -3,17 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="nl">
-      <Head>
-        <meta name="theme-color" content="#000000" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+     <Head>
+     <meta name="theme-color" content="#000000" />
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+     <link rel="manifest" href="/manifest.json" />
+     <link rel="apple-touch-icon" href="/icon-192.png" />
 
-        {/* Alleen iPhone 15 splash */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/splash/splash-iphone15.png"
-          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
-        />
+  {/* iPhone 15 splash only */}
+  <link
+    rel="apple-touch-startup-image"
+    href="/splash/splash-iphone15-optimized.png"
+    media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
+  />
+
       </Head>
       <body>
         <Main />
