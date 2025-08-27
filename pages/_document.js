2762598c83aee33254bb7043e0535b7ae90337f4
Jose Bennink <jose@mac.home>
2025-08-27 09:49:07 +0200

    Finale versie: splash + icons + manifest

--- pages/_document.js
+++ pages/_document.js
@@ -3,19 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="nl">
-     <Head>
-     <meta name="theme-color" content="#000000" />
-     <meta name="viewport" content="width=device-width, initial-scale=1" />
-     <link rel="manifest" href="/manifest.json" />
-     <link rel="apple-touch-icon" href="/icon-192.png" />
+      <Head>
+        {/* Basisinstellingen voor PWA */}
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
 
-  {/* iPhone 15 splash only */}
-  <link
-    rel="apple-touch-startup-image"
-    href="/splash/splash-iphone15-optimized.png"
-    media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
-  />
+        {/* App-icon voor iOS */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon-192.png" />
 
+        {/* Splashscreen iPhone 15 */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/splash/splash-iphone15-optimized.png"
+          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
+        />
       </Head>
       <body>
         <Main />
