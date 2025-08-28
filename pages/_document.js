d24ff5eca2c41a94e641fcc087018c63d8a5a80c
Jose Bennink <jose@mac.home>
2025-08-28 16:19:58 +0200

    Favicon/PWA icons vernieuwd; manifest opgeschoond; _document.js zonder viewport

--- pages/_document.js
+++ pages/_document.js
@@ -1,41 +1,18 @@
-// pages/_document.js
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="nl">
-        <Head>
-          {/* iOS/Android fullscreen en juiste insets */}
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, height=device-height, viewport-fit=cover"
-          />
-          <meta name="theme-color" content="#000000" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
-          {/* PRELOAD de juiste assets (PNG/PNG). Geen .jpeg hier. */}
-          <link rel="preload" as="image" href="/splash/splash-iphone15-v3.png" />
-          <link rel="preload" as="image" href="/splash/splash-desktop-1920x1080.png" />
-
-          {/* Optioneel: iOS startup images */}
-          <link
-            rel="apple-touch-startup-image"
-            href="/splash/splash-iphone15-v3.png"
-            media="(max-width: 768px)"
-          />
-          <link
-            rel="apple-touch-startup-image"
-            href="/splash/splash-desktop-1920x1080.png"
-            media="(min-width: 769px)"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="nl">
+      <Head>
+        <link rel="icon" href="/favicon.png?v=2" type="image/png" />
+        <link rel="apple-touch-icon" href="/icon-192.png?v=2" />
+        <link rel="manifest" href="/manifest.json?v=2" />
+        <meta name="theme-color" content="#0b2d26" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
